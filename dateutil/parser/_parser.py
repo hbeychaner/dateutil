13c3999de44061d546e9907c6b0d48c4557dd56f
Brandon Eychaner <42423779+hbeychaner@users.noreply.github.com>
2020-04-07 18:27:19 +0200

    Add IST tzinfo

--- dateutil/parser/_parser.py
+++ dateutil/parser/_parser.py
@@ -1208,7 +1208,7 @@ class parser(object):
             aware = naive
 
         elif res.tzname:
-            tz_dict = {"PDT": -7*60*60, "PST": -8*60*60, "EDT":-4*60*60}
+            tz_dict = {"PDT": -7*60*60, "PST": -8*60*60, "EDT":-4*60*60, "IST": 5.5*60*60}
             if res.tzname in tz_dict.keys():
                 aware = naive.replace(tzinfo=tz.offset(res.tzname, tz_dict[res.tzname]))
             else:
